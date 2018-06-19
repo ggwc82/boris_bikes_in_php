53cb83fb000b2bf35184fe08b36aabb2d7b76aaf
Godfrey Chiu <godfrey.chiu@catenamedia.com>
2018-06-19 14:52:23 +0100

    added test to check if method_exists for release_bike, PHPUnit doesn't appear to have responds_to equivalent

--- src/DockingStation.php
+++ src/DockingStation.php
@@ -1,5 +1,5 @@
 <?php
-use 
+
 class DockingStation
 {
 
--- tests/DockingStationTest.php
+++ tests/DockingStationTest.php
@@ -4,10 +4,23 @@ use PHPUnit\Framework\TestCase;
 require __DIR__ . "/../src/DockingStation.php";
 
 class DockingStationTest extends TestCase
-{
+{   
+    private $dockingstation;
+
+    public function setUp(){
+        $this->dockingstation = new DockingStation();
+    }
+
     public function test_describe_docking_station()
     {
-        $dockingstation = new DockingStation();
-        $this->assertInstanceOf('DockingStation', $dockingstation);
+        $this->assertInstanceOf('DockingStation', $this->dockingstation);
+    }
+
+    public function test_has_method_release_bike()
+    {
+        $this->assertTrue(
+            method_exists($this->dockingstation, 'release_bike'),
+            'Class doesn not have a method release_bike'
+        );
     }
 }
